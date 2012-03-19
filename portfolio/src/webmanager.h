8b9674d94784f3270c516932316114131cc4071f
Manuel Torres <jmtorresluc@gmail.com>
2012-03-19 20:52:53 -0300

    Clase WebManager.

---
+++ portfolio/src/webmanager.h
@@ -0,0 +1,19 @@
+#ifndef WEBMANAGER_H
+#define WEBMANAGER_H
+#include <QWebView>
+
+class WebManager : QObject
+{
+    Q_OBJECT
+public:
+    WebManager(QObject *parent = 0);
+    void getHandouts(int courseId);
+
+private:
+    QWebView *web;
+
+private slots:
+    void processPage(bool result);
+};
+
+#endif // WEBMANAGER_H
